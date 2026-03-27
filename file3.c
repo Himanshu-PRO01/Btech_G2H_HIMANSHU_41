694d5a55b0e5a0ae93630eaf38271efcd6a6b8a4
Himanshu <himan9shu4@gmail.com>
2026-03-27 11:27:20 +0530

    Add files via upload

---
+++ file3.c
@@ -0,0 +1,13 @@
+#include(stdio.h)
+#include(math.h)
+int main()
+{ int r;
+float area, circumference;
+printf("Entewr the radius of cirlce:";
+scanf("%d",r);
+area=3.14*r*r;
+circumference=2*3.14*r;
+printf("The area of the circle is %f,area");
+printf("The circumference of the circle %f,circumferenc");
+return 0;
+]
