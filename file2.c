dfc217f217664f4b586a5f1c9ba9eb4a2f4b47ae
Himanshu <himan9shu4@gmail.com>
2026-03-27 11:26:54 +0530

    Add files via upload

---
+++ file2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float p,r,t,SI,Amount,CI;
+    printf("Enter the principle amount:");
+    scanf("%d",&p);
+    printf("Enter the rate amount:");
+    scanf("%d",&r);
+    printf("Enter the time:");
+    scanf("%d",&t);
+    SI=(p*r*t)/100;
+    Amount=p*pow(1+R/100),t);
+    CI=Amount-p;
+    printf("The Si is %f,SI");
+    printf("The CI is %f,CI");
+    return 0;
+    
+}
