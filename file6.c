8c81e992d7761d559478b67aaca40157cd1f25f3
Himanshu <himan9shu4@gmail.com>
2026-03-27 11:27:58 +0530

    Add files via upload

---
+++ file6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ int firstnum,secondnum;
+printf("Enter the number to be checked :");
+scanf(("%d%d",&firstnum,&secondnum);
+if(firstnum == secondnum)
+{
+    printf("the entered numbers are equal ")
+}
+else
+{
+    printf("The number are not equal ");
+}
+return 0;
+
+}}
